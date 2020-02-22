6eff84133356167b00e2f06dc0901a14e500448b
Help <pczerokey@gmail.com>
2020-02-21 22:05:31 -0500

    Added default parameter values, got rid of excess variables

--- src/services/API.js
+++ src/services/API.js
@@ -1,19 +1,17 @@
 //TODO remove proxy at some point
 const PROXY = 'https://cors-anywhere.herokuapp.com/';
 const URLBEGINNING = 'https://query1.finance.yahoo.com/v7/finance/chart/';
-let urlMiddle = 'USDCUP';
-let stateData = {};
 const URLENDING = '=x?indicators=close&includeTimestamps=false&range=3mo&interval=1d';
 
 const API = {
-    getData: async(currency1, currency2) => {
+    getData: async(currency1='USD', currency2='CUP') => {
         try {
             await fetch(PROXY + URLBEGINNING + currency1 + currency2 + URLENDING, )
                 .then(data => data.json())
                 // .then(data => this.setState({data, loading: false}))
                 .then( (data) => {
                     console.log('FROM API.js', data);
-                    return data.data;
+                    return data;
                 }
             )
         }
