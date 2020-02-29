c8cfc9c92a96f957c7dc9e19e1121ddbd71e8c8c
Help <pczerokey@gmail.com>
2020-02-29 14:43:20 -0500

    move to one component, and remove unused imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import InputText from './Components/inputGroup'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {Col, Container, Row} from 'reactstrap';
+import {Container} from 'reactstrap';
 import RatesAPI from "./services/ratesAPI";
 import currencyAPI from "./services/currencyAPI";
 
@@ -24,7 +24,6 @@ function App(props) {
         for (const property in currencies) {
             currenciesArray.push(property);
         }
-        console.log("JESSI", currenciesArray[2]);
         currenciesArray.map(currency => {
             return currency;
         })
@@ -48,8 +47,7 @@ function App(props) {
                 newValue = amount * converter;
                 //TODO REVIew
                 //Makes input group re render loosing the selection
-                setnewValue(amount * converter)
-
+                // setnewValue(amount * converter)
                 console.log("Converted Value: ", newValue)
                 return newValue;
             }
@@ -84,32 +82,20 @@ function App(props) {
         fetchData();
     }, [currenciesArray]); // Or [] if effect doesn't need props or state
 
-    useEffect(() => {
-        console.log("****NEWBALUE IS***", newValue);
-    }, [newValue]);
-
     return (
         <div className="card">
             <Container>
                 {
                     randomFunction()
                 }
-                <Row>
-                    <Col>
-                        <InputText placeholder="From"
-                                   passCurrencyFunction={setFromCurrency}
+                        <InputText placeholderFrom="From"
+                                   placeholderTo="To"
+                                   passCurrencyToFunction={setFromCurrency}
+                                   passCurrencyFromFunction={setToCurrency}
                                    passAmount={getAmount}
                                    givenCurrencies={currenciesArray}
+                                   convertedValue={newValue}
                         />
-                    </Col>
-                    {/*<Col>*/}
-                    {/*    <InputText placeholder="To"*/}
-                    {/*               passCurrencyFunction={setToCurrency}*/}
-                    {/*               convertedValue={newValue}*/}
-                    {/*               givenCurrencies={currenciesArray}*/}
-                    {/*    />*/}
-                    {/*</Col>*/}
-                </Row>
             </Container>
         </div>
 
--- src/Components/inputGroup/index.js
+++ src/Components/inputGroup/index.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './style.css';
-import {FormGroup, Input, Label} from 'reactstrap';
+import {Col, FormGroup, Input, Label, Row} from 'reactstrap';
 
 const currencyFlag = require('currency-codes-ru-en-names');
 
@@ -10,17 +10,33 @@ const InputComponent = (props) => {
     let [myval, setdmyval] = useState();
     let [myCurrencies] = useState(props.givenCurrencies)
 
-    let changeDropDown = (newCurrency) => (
-        console.log("Currency set to " + newCurrency),
+    let changeDropDownTo = (newCurrency) => (
+        console.log("CurrencyTO set to " + newCurrency),
             setdDropDown(newCurrency),
             dropDown = newCurrency,
-            console.log("New currency is actually " + dropDown),
-            props.passCurrencyFunction(dropDown)
+            console.log("New currencyTO is actually " + dropDown),
+            props.passCurrencyToFunction(dropDown)
+
+    );
+
+    let changeDropDownFrom = (newCurrency) => (
+        console.log("CurrencyFROM set to " + newCurrency),
+            setdDropDown(newCurrency),
+            dropDown = newCurrency,
+            console.log("New currencyFROM is actually " + dropDown),
+            props.passCurrencyFromFunction(dropDown)
+
     );
 
     let sendAmount = async(e) => (
-        props.passAmount(e.target.value)
+        props.passAmount(e.target.value),
 
+        setTimeout(function()
+        { //Start the timer
+            // setdmyval(props.convertedValue);
+            console.log("*******THIS IS convertedValue: ", props.convertedValue);
+            //After 2 seconds, set render to true
+        }, 2000)
         //TODO Actually changes the value
         // await setdmyval(this.props.convertedValue),
         // console.log("******should have been set", props.convertedValue)
@@ -28,29 +44,61 @@ const InputComponent = (props) => {
 
 
     return (
-        <div>
-            <p>test {currencyFlag.names("USD", "en")}</p>
-            {/*{console.log("HERE",myCurrencies.length)}*/}
-            <FormGroup>
-                <Label>{props.placeholder} </Label>
-                <Input type="select" name="select" id="exampleSelect" onClick={(e) => changeDropDown(e.target.value)}>
-                    {
-                        props.givenCurrencies.map( (currency, key) => {
-                            console.log(currency + key + " - ", typeof currencyFlag.names(currency, "en") == 'string'? currencyFlag.names("" + currency, "en") : "test")
-                            // return <option key={key} value={currency}>{currency} - {currencyFlag.names("" + currency, "en")}</option>
-                        })
-                    }
+        <Row>
+            <Col>
+                {/*From*/}
+                <div>
+                    <p>Test? {currencyFlag.names("USD", "en")}</p>
+                    {/*{console.log("HERE",myCurrencies.length)}*/}
+                    <FormGroup>
+                        <Label>{props.placeholderFrom} </Label>
+                        <Input type="select" name="select" id="exampleSelect" onClick={(e) => changeDropDownTo(e.target.value)}>
+                            {
+                                props.givenCurrencies.map( (currency, key) => {
+                                    // console.log(currency + key + " - ", typeof currencyFlag.names(currency, "en") == 'string'? currencyFlag.names("" + currency, "en") : "test")
+                                    return <option key={key} value={currency}>{currency}</option>
+                                })
+                            }
 
-                </Input>
-            </FormGroup>
-            <hr className="my-2"/>
-            <div>
-                <Input type="text" className="amountText" placeholder={"Value"} onChange={(e) => sendAmount(e)}
-                       value={props.convertedValue}/>
-            </div>
-            <hr className="my-2"/>
+                        </Input>
+                    </FormGroup>
+                    <hr className="my-2"/>
+                    <div>
+                        <Input type="text" className="amountText" placeholder={"Value"} onChange={(e) => sendAmount(e)}
+                               // value={props.convertedValue}
+                        />
+                    </div>
+                    <hr className="my-2"/>
 
-        </div>
+                </div>
+            </Col>
+
+            {/*To*/}
+            <Col>
+                <div>
+                    <p>test {currencyFlag.names("USD", "en")}</p>
+                    {/*{console.log("HERE",myCurrencies.length)}*/}
+                    <FormGroup>
+                        <Label>{props.placeholderTo} </Label>
+                        <Input type="select" name="select" id="exampleSelect" onClick={(e) => changeDropDownFrom(e.target.value)}>
+                            {
+                                props.givenCurrencies.map( (currency, key) => {
+                                    // console.log(currency + key + " - ", typeof currencyFlag.names(currency, "en") == 'string'? currencyFlag.names("" + currency, "en") : "test")
+                                    return <option key={key} value={currency}>{currency}</option>
+                                })
+                            }
+
+                        </Input>
+                    </FormGroup>
+                    <hr className="my-2"/>
+                    <div>
+                        <Input disabled type="text" className="amountText" placeholder={"Converted Value"}
+                               value={myval}/>
+                    </div>
+                    <hr className="my-2"/>
+                </div>
+            </Col>
+        </Row>
     );
 };
 
