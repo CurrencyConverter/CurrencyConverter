12b2eaa49b7ecbfc3bf9e0c7bdba868a4c15be99
Help <pczerokey@gmail.com>
2020-02-29 20:26:54 -0500

    code cleanup/refactor

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
             <Container>
                 <InputText
                     placeholderFrom="From"
-                   placeholderTo="To"
+                    placeholderTo="To"
                 />
             </Container>
         </div>
--- src/Components/inputGroup/index.js
+++ src/Components/inputGroup/index.js
@@ -9,7 +9,7 @@ import currencyAPI from "../../services/currencyAPI";
 
 const InputComponent = (props) => {
     let [myval, setmyval] = useState();
-    let [fromCurrency, setfromCurrency] = useState("")
+    let [fromCurrency, setfromCurrency] = useState("");
     let [toCurrency, settoCurrency] = useState("");
     let currenciesObject = [];
     let amount = 0;
@@ -17,12 +17,12 @@ const InputComponent = (props) => {
 
     const getCurrencies = async () => {
         currenciesObject = await currencyAPI.getData();
-        randomFunction()
+        randomFunction();
         console.log('Currencies in this function', currencies);
     };
 
     function randomFunction() {
-        let currenciesArray = []
+        let currenciesArray = [];
         console.log("trackcall");
         for (const property in currenciesObject) {
             currenciesArray.push(property);
@@ -30,35 +30,32 @@ const InputComponent = (props) => {
 
         currenciesArray.map(currency => {
             return currency;
-        })
+        });
         console.log("Filled?", currenciesArray);
         setCurrencies(currenciesArray);
     }
 
     useEffect(() => {
-        // Update the document title using the browser API
-        getCurrencies();
-    },[]
+            // Update the document title using the browser API
+            getCurrencies();
+        }, []
     );
 
     let convertCurrency = async () => {
-        console.log("I HOPE THIS ISNT BEING CALLED AUTOMATICALLY")
-        console.log("FromCurrency: ", fromCurrency)
-        console.log("ToCurrency: ", toCurrency)
-        if(fromCurrency && toCurrency){
+        console.log("I HOPE THIS ISNT BEING CALLED AUTOMATICALLY");
+        console.log("FromCurrency: ", fromCurrency);
+        console.log("ToCurrency: ", toCurrency);
+        if (fromCurrency && toCurrency) {
             let res = await RatesAPI.getData(fromCurrency, toCurrency);
-            if(res.chart.result == null)
-            {
+            if (res.chart.result == null) {
                 //TODO SOME CONVERSION EXIST WHY ? IDK
                 console.log("THIS CONVERSION DOES NOT EXIST");
-            }
-            else
-            {
+            } else {
                 console.log("THE converter is being obtained", res);
                 let data = res.chart.result[0].indicators.quote[0].close;
                 let result = res.chart.result[0].indicators.quote[0].close[data.length - 1];
                 myval = result * amount;
-                setmyval(myval)
+                setmyval(myval);
                 console.log('Local myval is: ', myval);
             }
         }
@@ -67,22 +64,22 @@ const InputComponent = (props) => {
 
     let changeDropDownTo = (newCurrency) => (
         toCurrency = newCurrency,
-        settoCurrency(newCurrency),
-        console.log("Local toCurrency: ", toCurrency)
+            settoCurrency(newCurrency),
+            console.log("Local toCurrency: ", toCurrency)
     );
 
     let changeDropDownFrom = (newCurrency) => (
         fromCurrency = newCurrency,
-        setfromCurrency(newCurrency),
-        console.log("Local fromCurrency: ", fromCurrency)
+            setfromCurrency(newCurrency),
+            console.log("Local fromCurrency: ", fromCurrency)
     );
 
-    let sendAmount = async(e) => (
+    let sendAmount = async (e) => (
         amount = e.target.value,
-        await convertCurrency()
+            await convertCurrency()
     );
 
-    console.log("Why am I getting fucked now?", currencies)
+    console.log("Why am I getting fucked now?", currencies);
     return (
         <Row>
             <Col>
@@ -92,9 +89,10 @@ const InputComponent = (props) => {
                     {/*{console.log("HERE",myCurrencies.length)}*/}
                     <FormGroup>
                         <Label>{props.placeholderFrom} </Label>
-                        <Input type="select" name="select" id="exampleSelect" onClick={(e) => changeDropDownFrom(e.target.value)}>
+                        <Input type="select" name="select" id="exampleSelect"
+                               onClick={(e) => changeDropDownFrom(e.target.value)}>
                             {
-                                currencies.map( (currency, key) => {
+                                currencies.map((currency, key) => {
                                     // console.log(currency);
                                     return <option key={key} value={currency}>{currency}</option>
                                 })
@@ -105,7 +103,7 @@ const InputComponent = (props) => {
                     <hr className="my-2"/>
                     <div>
                         <Input type="text" className="amountText" placeholder={"Value"} onChange={(e) => sendAmount(e)}
-                               // value={props.convertedValue}
+                            // value={props.convertedValue}
                         />
                     </div>
                     <hr className="my-2"/>
@@ -120,9 +118,10 @@ const InputComponent = (props) => {
                     {/*{console.log("HERE",myCurrencies.length)}*/}
                     <FormGroup>
                         <Label>{props.placeholderTo} </Label>
-                        <Input type="select" name="select" id="exampleSelect" onClick={(e) => changeDropDownTo(e.target.value)}>
+                        <Input type="select" name="select" id="exampleSelect"
+                               onClick={(e) => changeDropDownTo(e.target.value)}>
                             {
-                                currencies.map( (currency, key) => {
+                                currencies.map((currency, key) => {
                                     return <option key={key} value={currency}>{currency}</option>
                                 })
                             }
@@ -131,7 +130,8 @@ const InputComponent = (props) => {
                     </FormGroup>
                     <hr className="my-2"/>
                     <div>
-                        <Input disabled type="text" id="converted" className="amountText" placeholder={"Converted Value"}
+                        <Input disabled type="text" id="converted" className="amountText"
+                               placeholder={"Converted Value"}
                                value={myval}/>
                     </div>
                     <hr className="my-2"/>
