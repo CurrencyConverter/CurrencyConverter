bb29e5dfd474cdf5a87cb076f131f2f3ddb2711d
Help <pczerokey@gmail.com>
2020-03-09 23:02:33 -0400

    Updates

---
+++ precache-manifest.c5916b63a4c669c7c58eff946a599be4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f28ec505610046f0ba451cefd356b2b",
+    "url": "/CurrencyConverter/index.html"
+  },
+  {
+    "revision": "ddce40baee2dde85e95a",
+    "url": "/CurrencyConverter/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6e9ce475088b6450207d",
+    "url": "/CurrencyConverter/static/css/main.91881dcb.chunk.css"
+  },
+  {
+    "revision": "ddce40baee2dde85e95a",
+    "url": "/CurrencyConverter/static/js/2.4ba6c7df.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/CurrencyConverter/static/js/2.4ba6c7df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9ce475088b6450207d",
+    "url": "/CurrencyConverter/static/js/main.37d6c226.chunk.js"
+  },
+  {
+    "revision": "86ddfdcd600fd211bfe5",
+    "url": "/CurrencyConverter/static/js/runtime-main.630b2ec6.js"
+  }
+]);
